2ca4eef6716bd7e9ddb65ffcc7ea4a4b7e744116
Oskar Wróbel <kontakt@oskarwrobel.pl>
2019-10-08 20:32:58 +0200

    Code refactoring.

--- src/deploy.js
+++ src/deploy.js
@@ -6,50 +6,127 @@ const shell = require( 'shelljs' );
 const NodeSSH = require( 'node-ssh' );
 
 /**
- * See README.
+ * Connects to te remote via SSH and allows for executing commands for the deploy purpose.
+ *
+ * See the README for more details.
+ *
+ * @param {Object} data
+ * @param {String} data.username
+ * @param {String} data.host
+ * @param {String} data.privateKey
+ * @param {Function} data.execute
  */
-module.exports = function deploy( { host, username, privateKey, execute } ) {
+module.exports = function deploy( { username, host, privateKey, execute } ) {
 	const ssh = new NodeSSH();
-	const commands = [];
+	const commands = new Set();
 
-	function local( command ) {
-		commands.push( { command } );
+	// Helper function for adding a local command to the commands stack.
+	function createLocalCommand( command ) {
+		commands.add( new Command( command ) );
 	}
 
-	function remote( command, options = {} ) {
-		commands.push( { command, options } );
+	// Helper function for adding a remote command to the commands stack.
+	function createRemoteCommand( command, options = {} ) {
+		commands.add( new Command( command, options ) );
 	}
 
-	execute( local, remote );
+	// Add all commands to the stack.
+	execute( createLocalCommand, createRemoteCommand );
 
+	log( `Connecting to the server ${ username }@${ host }.` );
+
+	// Connect to the server and execute commands from the stack one by one (in sequence).
 	return ssh.connect( { host, username, privateKey } )
-		.then( ssh => {
-			let chain = Promise.resolve();
+		.then( async ssh => {
+			log( 'Connection established.' );
 
-			for ( const { command, options } of commands ) {
-				chain = chain.then( () => {
-					console.log( options === undefined ? 'Local: ' : 'Remote: ', command );
-
-					if ( options === undefined ) {
-						shell.exec( command );
-					} else {
-						return ssh.execCommand( command, options ).then( output => {
-							if ( !options.silent ) {
-								if ( output.stderr.length ) {
-									return Promise.reject( output.stderr );
-								}
-
-								if ( output.stdout.length ) {
-									console.log( output.stdout );
-								}
-							}
-
-							return output.stdout;
-						} );
-					}
-				} );
+			for ( const command of commands ) {
+				await executeCommand( ssh, command );
 			}
 
-			return chain.then( () => ssh.connection.end() );
-		} );
+			log( 'Disconnecting from the server.' );
+		} )
+		.then( () => ssh.connection.end() )
+		.then( () => log( 'Disconnected.' ) );
 };
+
+/**
+ * Single command representation.
+ */
+class Command {
+	/**
+	 * @param {String} input
+	 * @param {Object} [options]
+	 */
+	constructor( input, options ) {
+		if ( typeof input !== 'string' ) {
+			throw new Error( 'Command has to be a string.' );
+		}
+
+		/**
+		 * @readonly
+		 * @type {string}
+		 */
+		this.input = input;
+
+		/**
+		 * @readonly
+		 * @type {Object}
+		 */
+		this.options = options;
+	}
+
+	/**
+	 * @type {String}
+	 */
+	get type() {
+		return this.options === undefined ? 'local' : 'remote';
+	}
+
+	/**
+	 * @type {Boolean}
+	 */
+	get isSilent() {
+		return this.options && this.options.silent;
+	}
+}
+
+/**
+ * Execute single command.
+ *
+ * @param {NodeSSH} ssh
+ * @param {Command} command
+ * @returns {Promise}
+ */
+async function executeCommand( ssh, command ) {
+	log( `Executing a ${ command.type } command: ${ command.input }` );
+
+	if ( command.type === 'local' ) {
+		shell.exec( command.input );
+
+		return Promise.resolve();
+	}
+
+	return ssh.execCommand( command.input, command.options ).then( output => {
+		if ( !command.isSilent ) {
+			if ( output.stderr.length ) {
+				return Promise.reject( output.stderr );
+			}
+
+			if ( output.stdout.length ) {
+				log( output.stdout );
+			}
+		}
+
+		return output.stdout;
+	} );
+}
+
+/**
+ * A helper function that prints given value on the console.
+ *
+ * @param {String} value
+ */
+function log( value ) {
+	console.log( value );
+}
