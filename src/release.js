ad95e4bf0038c1e426e58c04fdbf983043c7d05c
Oskar Wróbel <kontakt@oskarwrobel.pl>
2019-10-10 19:43:33 +0200

    Bumped version to 1.1.3.

--- src/release.js
+++ src/release.js
@@ -16,18 +16,18 @@ module.exports = async function release() {
 		message: `Choose the next version (current ${ version })`,
 		choices: [
 			{ title: `${ major }.${ minor }.${ patch + 1 }` },
-			{ title: `${ major }.${ minor + 1 }.${ patch }` },
-			{ title: `${ major + 1 }.${ minor }.${ patch }` }
+			{ title: `${ major }.${ minor + 1 }.0` },
+			{ title: `${ major + 1 }.0.0` }
 		]
 	} );
 
 	shell.exec( `npm version ${ response.version } -m "Bumped version to %s." -tag-version-prefix ""` );
+	shell.exec( 'npm publish' );
 	shell.exec( `git push --follow-tags` );
-	shell.exec( 'npm release' );
 
 	console.log(
 		`\nNew version ${ response.version } has been released.\n` +
-		'NPM package has been released.\n' +
+		'NPM package has been published.\n' +
 		'New tag has been pushed to the remote. '
 	)
 };
